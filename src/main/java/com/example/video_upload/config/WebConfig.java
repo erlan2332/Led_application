97e39144ce47d996f3ca7b4c8073ac74286c1afb
erlan2332 <134258930+erlan2332@users.noreply.github.com>
2025-03-03 15:16:52 +0600

    Update WebConfig.java

--- src/main/java/com/example/video_upload/config/WebConfig.java
+++ src/main/java/com/example/video_upload/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:3002") // Укажите адрес вашего фронтенда
+                .allowedOrigins("http://localhost:3002") 
                 .allowedMethods("GET", "POST", "DELETE", "PUT", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
